9f29349d1cd85f2a4f9fef1394664e03d8b215ae
Jon Mockbee <mockbee1000@gmail.com>
2021-07-19 20:40:17 -0400

    continuing on if

--- july19.py
+++ july19.py
@@ -1,14 +1,24 @@
-# Read two numbers
+# Read three numbers
 number1 = int(input("Enter the first number: "))
 number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
 
-# Choose the larger number
-if number1 > number2:
-    larger_number = number1
-else:
-    larger_number = number2
+# We temporarily assume that the first number
+# is the largest one.
+# We will verify this soon.
+largest_number = number1
+
+# We check if the second number is larger than current largest_number
+# and update largest_number if needed.
+if number2 > largest_number:
+    largest_number = number2
+
+# We check if the third number is larger than current largest_number
+# and update largest_number if needed.
+if number3 > largest_number:
+    largest_number = number3
 
 # Print the result
-print("The larger number is:", larger_number)
+print("The largest number is:", largest_number)
 
 
